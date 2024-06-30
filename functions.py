b3eb4f596d0495540e30f817afd9a06e84b2e1a0
alex-nightvision <alex@nightvision.net>
2024-06-30 16:50:17 -0400

    sast cannot detect this sql injection

--- functions.py
+++ functions.py
@@ -2,7 +2,13 @@ from flask import Flask, request, jsonify
 import requests
 
 def hide_from_sast(request):
-    name = request.args.get('name')
-    url = f"http://127.0.0.1:5000/users?name={name}"
+    # # shows Server-Side Request Forgery (SSRF) but this is not SQL Injection
+    # name = request.args.get('name')
+    # url = f"http://127.0.0.1:5000/users?name={name}"
+
+    # Hard coded not show up in semgrep at all
+    url = "http://127.0.0.1:5000/users?name='%20OR%20'1'='1"
+
+    # make request
     response = requests.get(url).json()
     return response
