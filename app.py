f2688995d2c438510e1038db7033acae1f95c5b3
alex-nightvision <alex@nightvision.net>
2024-06-30 17:25:22 -0400

    SAST fails 3 files deep

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import requests
-import functions
 import helper
 
 
@@ -8,7 +7,6 @@ app = Flask(__name__)
 
 @app.route('/users', methods=['GET'])
 def get_user():
-    # return functions.hide_from_sast(request)
     return helper.process_request(request)
 
 if __name__ == '__main__':
